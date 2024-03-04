cc839207e045d6ac9621dbd9cf3c64efcf004be7
Abdelrahman Abdelbaky <abderahmanmohammed99@gmail.com>
2024-03-04 19:24:10 +0300

    Made the code for the header file of register window

--- register.h
+++ register.h
@@ -4,19 +4,22 @@
 #include <QDialog>
 
 namespace Ui {
-class register;
+class Register;
 }
 
-class register : public QDialog
+class Register : public QDialog
 {
     Q_OBJECT
 
 public:
-    explicit register(QWidget *parent = nullptr);
-    ~register();
+    explicit Register(QWidget *parent = nullptr);
+    ~Register();
+
+private slots:
+    void on_pushButton_clicked();
 
 private:
-    Ui::register *ui;
+    Ui::Register *ui;
 };
 
 #endif // REGISTER_H
