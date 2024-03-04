f13d1f0a944f1be15c73c7c2209502e16b2768ab
Sama3mad <159120293+Sama3mad@users.noreply.github.com>
2024-03-04 18:26:57 +0200

    Update welcome.cpp

--- welcome.cpp
+++ welcome.cpp
@@ -1,14 +1,31 @@
 #include "welcome.h"
 #include "ui_welcome.h"
-
-welcome::welcome(QWidget *parent)
+#include "Users.h"
+#include "login.h"
+Welcome::Welcome(QWidget *parent)
     : QDialog(parent)
-    , ui(new Ui::welcome)
+    , ui(new Ui::Welcome)
 {
     ui->setupUi(this);
+    QPixmap pix ("C:\\Users\\Sama\\Downloads\\good morning.jpg");
+    int w = ui->Image->width();
+    int h = ui->Image->height();
+    ui->Image->setPixmap(pix.scaled (w,h, Qt:: KeepAspectRatio));
 }
 
-welcome::~welcome()
+Welcome::~Welcome()
 {
     delete ui;
 }
+
+void Welcome::setnameandage(QString username, int age){
+    ui->Hello->setText("Hello "+username+ " "+ QString :: number (age));
+}
+
+void Welcome::on_pushButton_clicked()
+{
+    hide();
+    Login *loginWindow = new Login (this);
+    loginWindow->show();
+}
+
